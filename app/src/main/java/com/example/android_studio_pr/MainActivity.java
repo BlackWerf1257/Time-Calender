1ebaffcf94a2bf90e4dff4ffaab1539b0d8e26b0
BlackWerf1257 <135800974+BlackWerf1257@users.noreply.github.com>
2024-05-18 22:26:46 +0900

    상단 UI 작업중

--- app/src/main/java/com/example/android_studio_pr/MainActivity.java
+++ app/src/main/java/com/example/android_studio_pr/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.android_studio_pr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.mainBtn);
     }
 }
