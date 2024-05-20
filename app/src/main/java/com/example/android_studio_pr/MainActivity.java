ee1e9a201f40da6cf9f564175cd86e5edb54e0e3
BlackWerf1257 <mklsjh44@gmail.com>
2024-05-21 00:21:56 +0900

    화면 크기 덜 구현됨

--- app/src/main/java/com/example/android_studio_pr/MainActivity.java
+++ app/src/main/java/com/example/android_studio_pr/MainActivity.java
@@ -4,15 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.widget.Button;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    Button btn;
+    Button[] topButtonList = new Button[5];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button) findViewById(R.id.mainBtn);
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        float density = getResources().getDisplayMetrics().density;
+        int width = (int)(metrics.widthPixels / density) / 5;
+
+        topButtonList[0] = (Button)findViewById(R.id.mainBtn);
+        topButtonList[1] = (Button)findViewById(R.id.calender);
+        topButtonList[2] = (Button)findViewById(R.id.schedule);
+        topButtonList[3] = (Button)findViewById(R.id.studyManage);
+        topButtonList[4] = (Button)findViewById(R.id.community);
+
+        for(int i=0; i< topButtonList.length; i++)
+            topButtonList[i].setWidth(width);
     }
 }
