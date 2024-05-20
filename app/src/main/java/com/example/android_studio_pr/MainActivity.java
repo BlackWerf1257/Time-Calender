435cf3a896f87dd0c71fcafdbd33749fe7cdebbf
BlackWerf1257 <mklsjh44@gmail.com>
2024-05-21 00:58:11 +0900

    상단바 및 로그인 창 UI 구현 완료

--- app/src/main/java/com/example/android_studio_pr/MainActivity.java
+++ app/src/main/java/com/example/android_studio_pr/MainActivity.java
@@ -11,26 +11,9 @@ import android.widget.Button;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    Button[] topButtonList = new Button[5];
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Display display = getWindowManager().getDefaultDisplay();
-        DisplayMetrics metrics = new DisplayMetrics();
-        display.getMetrics(metrics);
-        float density = getResources().getDisplayMetrics().density;
-        int width = (int)(metrics.widthPixels / density) / 5;
-
-        topButtonList[0] = (Button)findViewById(R.id.mainBtn);
-        topButtonList[1] = (Button)findViewById(R.id.calender);
-        topButtonList[2] = (Button)findViewById(R.id.schedule);
-        topButtonList[3] = (Button)findViewById(R.id.studyManage);
-        topButtonList[4] = (Button)findViewById(R.id.community);
-
-        for(int i=0; i< topButtonList.length; i++)
-            topButtonList[i].setWidth(width);
     }
 }
