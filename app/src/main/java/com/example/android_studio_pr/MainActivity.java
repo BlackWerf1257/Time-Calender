0f3e0dd172621c58b01361deb493b4ae635d31de
BlackWerf1257 <mklsjh44@gmail.com>
2024-05-23 00:34:10 +0900

    로그인/회원가입 구현 필요

--- app/src/main/java/com/example/android_studio_pr/MainActivity.java
+++ app/src/main/java/com/example/android_studio_pr/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.android_studio_pr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Display;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -15,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "실행됨", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, Dept_Announce.class);
+                startActivity(intent);
+            }
+        });
     }
 }
